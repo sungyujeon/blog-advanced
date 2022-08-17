8ed58790cebba363f2095e191846edabd8b54ab4
sungyujeon <sungyujeon91@gmail.com>
2022-08-17 19:43:01 +0900

    feat: hit

---
+++ src/main/java/com/blog/domain/Hit.java
@@ -0,0 +1,27 @@
+package com.blog.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class Hit {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private int count;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+
+}
